9c93508b2f6142d01203d6af3196b5d3a639109a
changodiez <joaquindiezw@gmail.com>
2020-06-01 12:02:45 +0200

    terminando css

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $('#submit').click(function (event) {
+   
+    console.log("Clicked button")
+
+
+    let email = $('#email').val()
+    let name = $('#name').val()
+    let mesagge = $('#message').val()
+    let statusElm = $('.status')
+    statusElm.empty();
+
+    if (email.length > 5 && email.includes('@') && email.includes('.')) {
+      statusElm.append("<div>Email is valid</div>")
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Email is not valid</div>')
+    }
+    if (subject.length >= 2) {
+     
+      statusElm.appendstatusElm.append('<div>Subject is valid</div>')
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Subject is not valid</div>')
+    }
+    if (MessageChannel.length >= 5){
+      statusElm.append('<div>Message is valid</div>')
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Message is not valid</div>')
+    }
+  })
+})
